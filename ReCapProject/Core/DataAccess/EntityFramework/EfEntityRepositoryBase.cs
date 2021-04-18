b3ec41a063b47b1fd43717a483667c179187641d
ilkaybsraydin@hotmail.com <ilkaybsraydin@hotmail.com>
2021-04-18 22:20:15 +0300

    WebAPI katmanı oluşturuldu

--- ReCapProject/Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
+++ ReCapProject/Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -43,10 +43,12 @@ namespace Core.DataAccess.EntityFramework
 
         public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
         {
-            using (TContext context = new TContext())
-            {
-                return filter == null ? context.Set<TEntity>().ToList() : context.Set<TEntity>().Where(filter).ToList();
-            }
+
+                using (TContext context = new TContext())
+                {
+                    return filter == null ? context.Set<TEntity>().ToList() : context.Set<TEntity>().Where(filter).ToList();
+                }
+            return null;
         }
 
         public void Update(TEntity entity)
