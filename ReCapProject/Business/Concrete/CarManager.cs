4cbea385412d2f6b40b65cc0552d9df3f58d9464
ilkaybsraydin@hotmail.com <ilkaybsraydin@hotmail.com>
2021-04-20 15:23:04 +0300

    Business katmanında ki tüm servislerim API karşılığı yazıldı

--- ReCapProject/Business/Concrete/CarManager.cs
+++ ReCapProject/Business/Concrete/CarManager.cs
@@ -38,7 +38,7 @@ namespace Business
 
         public IDataResult<List<Car>> GetAll()
         {
-            if (DateTime.Now.Hour==10)
+            if (DateTime.Now.Hour==18)
             {
                 return new ErrorDataResult<List<Car>>(Messages.MaintenanceTime);
             }
--- ReCapProject/WebAPI/Controllers/UsersController.cs
+++ ReCapProject/WebAPI/Controllers/UsersController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concrete;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +13,65 @@ namespace WebAPI.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
+        IUserService _userservice;
+        public UsersController(IUserService userService) 
+        {
+            _userservice = userService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll() 
+        {
+            var result = _userservice.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id) 
+        {
+            var result = _userservice.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(User user) 
+        {
+            var result = _userservice.Add(user);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPut("update")]
+        public IActionResult Update(User user) 
+        {
+            var result = _userservice.Update(user);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult Delete(User user) 
+        {
+            var result = _userservice.Delete(user);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
     }
 }
